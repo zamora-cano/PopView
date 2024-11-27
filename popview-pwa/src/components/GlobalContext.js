0c54d8df7131d50f901c623d98e302ba17f3c34f
Angel Zamora <angelzamoracano@gmail.com>
2024-11-27 10:36:10 -0600

    configurando para que las urls sean los mismos de backend

--- popview-pwa/src/components/GlobalContext.js
+++ popview-pwa/src/components/GlobalContext.js
@@ -5,7 +5,8 @@ const GlobalContext = createContext();
 export const GlobalProvider = ({ children }) => {
 /* URL Locales */
   const urlBackend = 'http://127.0.0.1:8000';
-  const urlFront = 'http://localhost:3000';
+  // const urlFront = 'http://localhost:3000';
+  const urlFront = 'http://127.0.0.1:8000';
 
   // const urlBackend = 'https://50ee-187-212-116-110.ngrok-free.app';
   // const urlFront = 'http://172.31.99.238:3000';
